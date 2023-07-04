b93cf0559b504a11a80c8d7ef765639d88e08b8e
Muhindo Mubaraka <mubs0x@gmail.com>
2023-07-04 03:25:59 +0300

    🥰

--- resources/views/layouts/layout-main.blade.php
+++ resources/views/layouts/layout-main.blade.php
@@ -265,7 +265,7 @@
             <p class="nav d-block  text-center text-md-start pb-2 pb-lg-0 mb-0">
                 Hand-made with ❤️ by
                 <a class="nav-link d-inline-block p-0" href="https://twitter.com/8TechConsults" target="_blank"
-                    rel="noopener">8Technologies 🥰</a>
+                    rel="noopener">8Technologies Consults</a>
             </p>
         </div>
     </footer>
--- vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
+++ vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
@@ -17,6 +17,6 @@
     <p class="nav d-block    text-md-start pb-2 pb-lg-0 mb-0">
         Hand-made with ❤️ by
         <b><a class="nav-link d-inline-block p-0 text-primary" href="https://twitter.com/8TechConsults"
-            target="_blank" rel="noopener">8Technologies 🥰</a></b>
+            target="_blank" rel="noopener">8Technologies Consults</a></b>
     </p>
 </footer>
