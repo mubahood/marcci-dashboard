680691cf78098b709df65a492ef925b9aa1c4c9f
Muhindo Mubaraka <mubs0x@gmail.com>
2023-09-04 01:28:45 +0300

    im

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -457,7 +457,10 @@ administrator_id
             ) {
                 $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
                 $file_name = time() . "-" . rand(100000, 1000000) . "." . $ext;
-                $destination = Utils::docs_root() . '/storage/images/' . $file_name;
+                //$destination = Utils::docs_root() . '/storage/images/' . $file_name;
+
+                $root_path = $_SERVER['DOCUMENT_ROOT'];
+                $destination = $root_path.'/public/storage/images' . $file_name;
 
                 $res = move_uploaded_file($file['tmp_name'], $destination);
                 if (!$res) {
