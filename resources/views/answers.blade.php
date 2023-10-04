fccec1987285161fb2fc9a61f292211ad3e0c2cd
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2023-10-04 15:45:59 +0300

    chat

--- resources/views/answers.blade.php
+++ resources/views/answers.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,8 +16,15 @@
 </head>
 <body>
     <div class="container mt-4">
+       
         <div class="card">
+       
             <div class="card-header">
+            <a href="javascript:history.back()" class="back-link">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M11.354 2.646a.5.5 0 0 1 0 .708L7.707 7H14.5a.5.5 0 0 1 0 1H7.707l3.647 3.646a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708 0z"/>
+            </svg> Back
+        </a>
                 <h1>{{$question}}</h1>
             </div>
             <div class="card-body">
@@ -27,7 +33,6 @@
                 @php
                  $user = App\Models\User::find($answer->user_id)->name;
                  $date = $answer->created_at->format('Y-m-d'); // Format it as 'YYYY-MM-DD HH:MM:SS'
-
                 @endphp
                 <div class="answer_list">
                     <h4>{{ $answer->answer }}</h4>
@@ -40,5 +45,3 @@
     </div>
 </body>
 </html>
-
-
