25ea8c84e070ee0ec743bafa5082a6d1e01cb027
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2024-01-22 09:16:59 +0300

    pestsanddidiseases

--- app/Admin/Controllers/PestsAndDiseaseController.php
+++ app/Admin/Controllers/PestsAndDiseaseController.php
@@ -57,9 +57,9 @@ class PestsAndDiseaseController extends AdminController
         $grid->column('user_id', __('User'))->display(function($user_id) {
             return \App\Models\User::find($user_id)->name;
         });
-        // $grid->column('variety_id', __('Variety'))->display(function($variety_id) {
-        //     return \App\Models\GroundnutVariety::find($variety_id)->name;
-        // });
+        $grid->column('variety_id', __('Variety'))->display(function($variety_id) {
+            return \App\Models\GroundnutVariety::find($variety_id)->name;
+        });
         $grid->column('category', __('Category'));
   
         return $grid;
@@ -79,9 +79,9 @@ class PestsAndDiseaseController extends AdminController
         $show->field('user_id', __('User id'))->as(function($user_id){
             return \App\Models\User::find($user_id)->name;
         });
-        // $show->field('variety_id', __('Variety'))->as(function($variety_id){
-        //     return \App\Models\GroundnutVariety::find($variety_id)->name;
-        // });
+        $show->field('variety_id', __('Variety'))->as(function($variety_id){
+            return \App\Models\GroundnutVariety::find($variety_id)->name;
+        });
         $show->field('category', __('Category'));
         $show->field('photo', __('Photo'))->image();
         $show->field('video', __('Video'))->video();
@@ -118,105 +118,7 @@ class PestsAndDiseaseController extends AdminController
             return redirect('/pests-and-diseases');
         });
 
-     
-// $form->saving(function (Form $form) {
-//     // Use request() to get the submitted value
-//     $audioInput = request('audioInput');
-//     error_log('audioInput: ' . $audioInput);
 
-//     if ($audioInput) {
-//         $audioExtension = pathinfo($audioInput, PATHINFO_EXTENSION);
-//         $audioName = 'audio_' . time() . '.' . $audioExtension;
-//         $audioBinary = base64_decode(str_replace('data:audio/' . $audioExtension . ';base64,', '', $audioInput));
-
-//         // Save the file to storage using UploadedFile
-//         $tempFilePath = tempnam(sys_get_temp_dir(), 'audio_temp_');
-//         file_put_contents($tempFilePath, $audioBinary);
-        
-//         $uploadedFile = new UploadedFile(
-//             $tempFilePath,
-//             $audioName,
-//             'audio/' . $audioExtension,
-//             null,
-//             true // Set to true to enable "test" mode
-//         );
-
-//         Storage::disk('public')->put($audioName, $uploadedFile->getContent());
-
-//         // Update the form field value
-//         $form->audio = $audioName;
-
-//         // Delete the temporary file
-//         unlink($tempFilePath);
-//     }
-// });
-        
-       
-
-       
-
-      
-    //     $form->html('<button type="button" id="startRecordingButton">Start Recording</button>');
-    //     $form->html('<button type="button" id="stopRecordingButton" style="display: none;">Stop Recording</button>');
-    //     $form->html('<audio id="audioPlayer" controls></audio>');
-    //     $form->html('<input type="hidden" id="audioInput" name="audioInput">');
-         $form->file('audio', __('Audio Input'));
-    //     $form->html('<script>
-    //     var startRecordingButton = document.getElementById("startRecordingButton");
-    //     var stopRecordingButton = document.getElementById("stopRecordingButton");
-    //     var audioPlayer = document.getElementById("audioPlayer");
-    //     var audioInput = document.getElementById("audioInput");
-    //     var mediaRecorder;
-    //     var audioChunks = [];
-        
-    //     startRecordingButton.addEventListener("click", startRecording);
-    //     stopRecordingButton.addEventListener("click", stopRecording);
-        
-    //     function startRecording() {
-    //         navigator.mediaDevices.getUserMedia({ audio: true })
-    //             .then(function (stream) {
-    //                 mediaRecorder = new MediaRecorder(stream);
-        
-    //                 mediaRecorder.ondataavailable = function (event) {
-    //                     if (event.data.size > 0) {
-    //                         audioChunks.push(event.data);
-    //                     }
-    //                 };
-        
-    //                 mediaRecorder.onstop = function () {
-    //                     var audioBlob = new Blob(audioChunks, { type: "audio/wav" });
-        
-    //                     // Convert the Blob to an audio file
-    //                     const audioUrl = URL.createObjectURL(audioBlob);
-        
-    //                     // Use the audioUrl for playback or other purposes if needed
-    //                     audioPlayer.src = audioUrl;
-        
-    //                     // Create FormData and append the audioBlob
-    //                     var formData = new FormData();
-    //                     formData.append("audioInput", audioBlob, "audio.wav");
-        
-    //                     // Update the value of audioInput
-    //                     audioInput.value = "audio.wav";
-    //                 };
-        
-    //                 mediaRecorder.start();
-    //                 startRecordingButton.style.display = "none";
-    //                 stopRecordingButton.style.display = "block";
-    //             })
-    //             .catch(function (error) {
-    //                 console.error("Error accessing microphone:", error);
-    //             });
-    //     }
-        
-    //     function stopRecording() {
-    //         mediaRecorder.stop();
-    //         startRecordingButton.style.display = "block";
-    //         stopRecordingButton.style.display = "none";
-    //     }
-        
-    // </script>');
-    
 
      
         //add a get gps coordinate button
@@ -241,14 +143,14 @@ class PestsAndDiseaseController extends AdminController
                 }
             });
         SCRIPT);
-        //$form->select('variety_id', __('Select crop variety '))->options(GroundnutVariety::pluck('name', 'id'))->rules('required');
+        $form->select('variety_id', __('Select crop variety '))->options(GroundnutVariety::pluck('name', 'id'))->rules('required');
         $form->text('category', __('Select Inquiries category'))->options([
             'Extension'=>'Extension',
             'Query'=>'Query',
         ]);
         $form->file('photo', __('Photo'));
         $form->file('video', __('Video'));
-        //$form->hidden('audio', __('Audio'));
+        $form->file('audio', __('Audio'));
 
         $form->text('description', __('Description'));
 
